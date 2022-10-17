569495338095292c37e053858bd0b451478fcaa6
mzahi <mzahi@DESKTOP-6QU8FCL>
2022-10-18 02:38:43 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 200;
+            double faizOrani = 1.5;
+            bool sistemeGirişYapmışMı = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.65;
+
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirişYapmışMı == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+           
+            
+
+        }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // array - dizi
+            string[] kurslar = new string[]{ "Yazılım Geliştirici Yetiştirme Kampı", "Programlamay Başlangıç İçin Temel Kurs", "Java", "Phyton", "C++", "Java Script" };
+
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For Bitti");
+
+
+            // Foreach Dizi temelli yapıları tek tek dönmeye yarar ve Dizilere uygulanır...
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+        }
+    }
+}
